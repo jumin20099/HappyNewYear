4c0bd725c9504567f7d0bb38f6724d9bcd51d829
jumin20099 <jumin20099@gmail.com>
2023-12-31 15:46:52 +0900

    새해복많이받으세요

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   // 초기화면에서는 기본 검색어로 설정
+
   searchImages('불꽃놀이');
 });
 
@@ -24,21 +25,21 @@ function moveImageContainerRandomly() {
 
 async function searchImages() {
   moveImageContainerRandomly();
-
+  
   const userSearchQuery = '불꽃놀이';
   const encodedSearchQuery = encodeURIComponent(userSearchQuery);
-
+  
   try {
     // 이미지 검색 API에 요청을 보냄
     const response = await fetch(`http://localhost:3000/search-images?query=${encodedSearchQuery}`);
     const data = await response.json();
-
+    
     // 이미지 배열을 무작위로 섞음
     const shuffledImages = shuffleArray(data.items);
-
+    
     // 특정 개수(예: 10개)의 무작위 이미지를 선택
     const newImages = shuffledImages.slice(0, 10);
-
+    
     // API 응답을 이용하여 원하는 작업을 수행
     displayImages(newImages);
   } catch (error) {
@@ -46,6 +47,18 @@ async function searchImages() {
     console.error(error);
   }
 }
+
+document.getElementById('fireWorksBtn').addEventListener('click', () => {
+  const audio3 = document.getElementById('realFireWorksSound');
+  const audio2 = document.getElementById('mainFireWorks');
+  audio3.volume = 1;
+  audio3.currentTime = 0;
+  audio3.play();
+  audio2.volume = 1;
+  audio2.autoplay = true;
+  audio2.loop = true;
+  audio2.play();
+})
 
 // 피셔 예이츠 셔플 알고리즘을 사용하여 배열을 무작위로 섞는 함수
 function shuffleArray(array) {
@@ -92,3 +105,25 @@ function displayImages(images) {
     console.error('Images data is not an array:', images);
   }
 }
+
+function 환호하는소리재생(){
+  const audio1 = document.getElementById('hwanho');
+  audio1.volume = 1;
+  audio1.currentTime = 0;
+  audio1.play();
+}
+
+function 반짝반짝(){
+  const body = document.body;
+  const currentColor = getComputedStyle(body).backgroundColor;
+  body.style.backgroundColor = currentColor === 'rgb(255, 255, 255)' ? 'black' : 'white';
+}
+
+
+// document.addEventListener('DOMContedtLoaded', () => {
+//   const audio2 = document.getElementById('mainFireWorks')
+//   audio2.volume = 1;
+//   audio2.autoplay = true;
+//   audio2.loop = true;
+//   audio2.play();
+// });
